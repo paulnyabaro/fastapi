3a295176c0dfed830eb21f71506ba3435a26a1e4
barrowpaul <barrowpaul738@gmail.com>
2022-09-24 08:26:22 +0300

    Loading content from POST request

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my first API app"}
+
+@app.get("/posts")
+def get_post():
+    return {"post1": "This is just the first post"}
+
+@app.post("/createpost")
+def create_post(post_content: dict = Body(...)):
+    print(post_content)
+    return {"message": "Post created successfully"}
