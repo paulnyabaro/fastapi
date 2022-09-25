5153cb6154a0ba94e3ebddd02a5398debce3d476
barrowpaul <barrowpaul738@gmail.com>
2022-09-25 18:06:00 +0300

    Small helper comment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.params import Body
 
 app = FastAPI()
 
+# routes
 @app.get("/")
 async def root():
     return {"message": "Welcome to my first API app"}
@@ -14,4 +15,4 @@ def get_post():
 @app.post("/createpost")
 def create_post(post_content: dict = Body(...)):
     print(post_content)
-    return {"message": "Post created successfully"}
+    return {"new_post": f"title: {post_content['title']}, content: {post_content['content']}"}
